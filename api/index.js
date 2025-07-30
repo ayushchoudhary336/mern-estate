4bfbfef3336812f9f6e5b60a003d98867fddf60f
Ayush <ayushchoudhary082003@gmail.com>
2025-07-31 01:41:04 +0530

    create api routes and controllers for user management

--- api/index.js
+++ api/index.js
@@ -1,14 +1,19 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.routes.js";
+
 dotenv.config();
 
 const app = express();
+app.use(express.json());
 
 mongoose
   .connect(process.env.MONGO)
   .then(() => console.log("MongoDB connected!"))
   .catch((err) => console.error("MongoDB connection error:", err));
+
+app.use("/api/user", userRouter);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000!");
